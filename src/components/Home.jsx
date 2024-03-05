798a94bdba3a560a534a3c28b69649df50ab9177
brandon-medina <brandon.k.medina@outlook.com>
2024-03-04 19:03:44 -0600

    Setup Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import home_store from '../assets/img/home_store.jpg'
+function Home() {
+    return (
+    <div>
+        <h1>Welcome to the Fake Store</h1>
+        <img src={home_store}/>
+    </div>);
+  }
+  
+  export default Home;
